6777a2537f3be6ca5849420f2fded5e3205d50fa
mdellit110 <matt.dellitalia@gmail.com>
2019-05-01 15:13:28 -0700

    cards have two sides and flip with css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Card = props => {
-  const { color } = props;
-  return <div className={`${color}`} />;
-};
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "down"
+    };
+  }
+  flipCard(card) {
+    const { active } = this.state;
+    active === "down"
+      ? this.setState({
+          active: "up"
+        })
+      : this.setState({
+          active: "down"
+        });
+  }
+
+  render() {
+    const { color, card } = this.props;
+    const { active } = this.state;
+    return (
+      <div
+        onClick={() => this.flipCard(card)}
+        className={`card ${color} ${active}`}
+      >
+        <div className="back">
+          <p>Cards Against Humanity</p>
+        </div>
+        <div className="front">
+          <p>{card}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Card;
--- src/Components/Deck.jsx
+++ src/Components/Deck.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Card from "./Card";
 
 const Deck = props => {
-  const { color } = props;
+  const { color, deck, flip } = props;
   return (
     <div className={`${color} deck`}>
-      <Card color={color} />
+      {deck.map(card => (
+        <Card flip={flip} card={card} color={color} />
+      ))}
     </div>
   );
 };
